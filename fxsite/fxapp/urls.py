accda0c8aa1384e330513cd55bdf94b53f9a8938
diarmuidclarke <32582190+diarmuidclarke@users.noreply.github.com>
2021-05-01 13:03:27 +0100

    basic web app working

--- fxsite/fxapp/urls.py
+++ fxsite/fxapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'fxapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('fxsubmit/', views.FileWorx_Submit, name='fxsubmit'),
 ]
--- fxsite/fxapp/views.py
+++ fxsite/fxapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def FileWorx_Submit(request):
+
+    context = {
+        'title': 'lets be having those files now',
+    }
+
+    return render(request, 'fxapp/fx_submit.html', context)
